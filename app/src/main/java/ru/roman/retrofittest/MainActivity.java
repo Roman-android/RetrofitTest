de10c1146a2c210290a781366f1b4e1b931a6ca0
Roman Malkov <roman-li-83@yandex.ru>
2018-07-07 21:54:26 +0500

    Initial commit. Show "size" in log panel.

---
+++ app/src/main/java/ru/roman/retrofittest/MainActivity.java
@@ -0,0 +1,86 @@
+package ru.roman.retrofittest;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                download();
+            }
+        });
+
+
+    }
+
+    private void download(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://rawgit.com/startandroid/data/master/messages/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        MessagesApi messagesApi = retrofit.create(MessagesApi.class);
+
+        Call<List<Message>> messages = messagesApi.messages();
+        messages.enqueue(new Callback<List<Message>>() {
+            @Override
+            public void onResponse(Call<List<Message>> call, Response<List<Message>> response) {
+                Log.d("OnResponse", "response "+response.body().size());
+            }
+
+            @Override
+            public void onFailure(Call<List<Message>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/ru/roman/retrofittest/Message.java
@@ -0,0 +1,9 @@
+package ru.roman.retrofittest;
+
+public class Message {
+
+    private long id;
+    private long time;
+    private String text;
+    private String image;
+}
